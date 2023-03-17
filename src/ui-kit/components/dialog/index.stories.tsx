0a5746cbdf58e6213913b93e4e2a9cb5d35d9f8e
zhanheng <zhanheng@agora.io>
2023-03-17 11:02:57 +0800

    feat: dialog

--- src/ui-kit/components/dialog/index.stories.tsx
+++ src/ui-kit/components/dialog/index.stories.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useState } from 'react';
 import { Meta } from '@storybook/react';
 import { FcrDialog } from '.';
+import { FcrButton } from '../button';
 const meta: Meta = {
   title: 'Components/Dialog',
 };
@@ -9,8 +10,10 @@ export const Docs = (props) => {
   const [visible, setVisible] = useState(false);
   return (
     <div>
-      <button onClick={() => setVisible(true)}>open dialog</button>
-      <FcrDialog visible={visible} onClose={() => setVisible(false)}></FcrDialog>;
+      <FcrButton onClick={() => setVisible(true)}>open dialog</FcrButton>
+      <FcrDialog title={'title'} visible={visible} onClose={() => setVisible(false)}>
+        content
+      </FcrDialog>
     </div>
   );
 };
--- src/ui-kit/components/dialog/index.tsx
+++ src/ui-kit/components/dialog/index.tsx
@@ -1,14 +1,49 @@
+import { themeVal } from '@onlineclass/ui-kit/tailwindcss';
+import classNames from 'classnames';
 import RcDialog from 'rc-dialog';
-import 'rc-dialog/assets/index.css';
+import './index.css';
+import { FC, ReactNode } from 'react';
+import { FcrButton } from '../button';
+import { SvgIconEnum, SvgImg } from '../svg-img';
+const colors = themeVal('colors');
+
 interface FcrDialogProps {
   visible?: boolean;
   onClose?: () => void;
+  title?: ReactNode;
+  footer?: ReactNode;
+  onOk?: () => void;
+  closable?: boolean;
 }
-export const FcrDialog = (props: FcrDialogProps) => {
-  const { visible, onClose } = props;
+export const FcrDialog: FC<FcrDialogProps> = (props) => {
+  const { visible, onClose, children, title, closable, footer } = props;
   return (
-    <RcDialog animation={'zoom'} maskAnimation={'fade'} visible={visible} onClose={onClose}>
-      <div>dialog</div>
+    <RcDialog
+      footer={null}
+      prefixCls="fcr-dialog"
+      animation={'zoom'}
+      maskAnimation={'fade'}
+      closeIcon={
+        <div className={classNames('fcr-dialog-close-override')}>
+          <SvgImg
+            type={SvgIconEnum.FCR_CLOSE}
+            size={18}
+            colors={{ iconPrimary: colors['notsb-inverse'] }}></SvgImg>
+        </div>
+      }
+      closable={closable}
+      visible={visible}
+      onClose={onClose}>
+      <div className={classNames('fcr-dialog-title')}>{title}</div>
+      <div className={classNames('fcr-dialog-inner')}>{children}</div>
+      <div className={classNames('fcr-dialog-footer')}>
+        {footer || (
+          <div className={classNames('fcr-dialog-footer-btns')}>
+            <FcrButton size="S">Ok</FcrButton>
+            <FcrButton size="S">Cancel</FcrButton>
+          </div>
+        )}
+      </div>
     </RcDialog>
   );
 };
