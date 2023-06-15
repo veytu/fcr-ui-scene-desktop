f977a591fe68353224a8d1f813fddb59d90149c8
zhanheng <zhanheng@agora.io>
2023-06-15 18:17:09 +0800

    fix: tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 const tailwindConfig = require('./tailwind.config');
-
 module.exports = {
-  plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
+  plugins: [tailwindcss(tailwindConfig), autoprefixer()],
 };
