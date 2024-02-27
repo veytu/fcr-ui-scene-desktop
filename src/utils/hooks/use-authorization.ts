11395298beb38f0d756588b6df61d910b57787ed
caoleiqing <17612175172@163.com>
2024-02-27 23:20:15 +0800

    feat: 修改权限控制

--- src/utils/hooks/use-authorization.ts
+++ src/utils/hooks/use-authorization.ts
@@ -1,12 +1,16 @@
 import { ToastApi } from '@components/toast';
 import { useStore } from './use-store';
 import { useI18n } from 'agora-common-libs';
+import { isH5 } from '@ui-scene/containers/participants';
 
 export const useAuthorization = (userUuid: string) => {
   const {
     presentationUIStore: { isBoardWidgetActive },
     boardApi: { grantedUsers, grantPrivilege },
     statusBarUIStore: { isHost },
+    participantsUIStore: {
+      participantTableList
+    },
   } = useStore();
   const transI18n = useI18n();
   const granted = grantedUsers.has(userUuid);
@@ -23,6 +27,19 @@ export const useAuthorization = (userUuid: string) => {
             closable: true,
             type: 'error',
             content: transI18n('fcr_room_tips_authorize_open_whiteboard'),
+          },
+        });
+      const user = participantTableList.filter((participant) => {
+        return participant.user.userUuid === userUuid
+      })
+      if (isH5(user[0]?.user))
+        return ToastApi.open({
+          persist: true,
+          duration: 15000,
+          toastProps: {
+            closable: true,
+            type: 'error',
+            content: transI18n('fcr_participants_tips_device_mobile_web_not_support'),
           },
         });
       grantPrivilege(userUuid, !granted);
